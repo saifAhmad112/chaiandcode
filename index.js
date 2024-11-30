0a23112c1c8531e37dc3a3eff59a1b1cc1d7596b
saifAhmad112 <saif.sid6@gmail.com>
2024-11-30 13:05:32 +0530

    add modal

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors');
+
 const app = express()
 const port = 5000
+
+const jsonFIle=[
+    {
+        title:"node",
+        amout:"11"
+    },
+    {
+        title:"express",
+        amout:"10"
+    },
+    {
+        title:"express",
+        amout:"122"
+    },
+]
+
+app.use(cors({ origin: '*' }));
+// app.use(cors({ origin: 'http://localhost:3000' }));
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,6 +34,14 @@ app.get('/', (req, res) => {
 
 app.get('/youtube', (req, res)=>{
     res.send("hello youtube")
+})
+
+app.get("/json",(req,res)=>{
+    res.json(jsonFIle)
+})
+
+app.get("/nodemon",(req,res)=>{
+    res.send("nodemon is running")
 })
 
 app.listen(process.env.PORT, () => {
